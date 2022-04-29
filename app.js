3f6a0954c6beb5426319e584d1faebfecc8ddf8a
mr-wang1009 <1115378579@qq.com>
2022-04-29 16:28:05 +0800

    添加bin目录

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
-const app = require('express')();
-const port = 3030;
-const index = require('./router/index');
-var bodyParser = require('body-parser')
-const login = require('./router/common/login');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var login = require('./router/common/login')
+var app = express();
 //设置跨域访问 -- 开始 --
-// app.all('*', function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*'); //的允许所有域名的端口请求（跨域解决）
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     res.header('Access-Control-Allow-Methods', '*');
-//     res.header('Content-Type', 'application/json;charset=utf-8');
-//     if (req.method === 'OPTIONS') {
-//         res.end();
-//     } else {
-//         next();
-//     }
-// });
-// app.use(bodyParser.urlencoded({ extended: false }));
-//设置跨域访问 -- 结束 --
-// 接口名称
-app.use('/', index);
+// app.all('*', function(req, res, next) {
+// res.header('Access-Control-Allow-Origin', '*') //的允许所有域名的端口请求（跨域解决）
+// res.header('Access-Control-Allow-Headers', 'Content-Type')
+// res.header('Access-Control-Allow-Methods', '*')
+// res.header('Content-Type', 'application/json;charset=utf-8')
+// if (req.method === 'OPTIONS') {
+//     res.end()
+// } else {
+//     next()
+// }
+// })
+// app.use(bodyParser.urlencoded({ extended: false }))
+  //设置跨域访问 -- 结束 --
+  // 接口名称
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/login', login);
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
